e99b430351b1a83b9a5426d642fcdb9c61c71fbd
abhisek kumar <abhisekkumarjaiswal23@gmail.com>
2025-03-14 01:15:40 +0530

    updated

--- backend/jwt/AuthToken.js
+++ backend/jwt/AuthToken.js
@@ -6,9 +6,9 @@ const createTokenAndSaveCookies = async (userId, res) => {
     expiresIn: "7d",
   });
   res.cookie("jwt", token, {
-    httpOnly: true, // Temporarily set to false for testing
+    httpOnly: false, // Temporarily set to false for testing
     secure: true,
-    sameSite: "strict",
+    sameSite: "none",
     path: "/", // Ensure the cookie is available throughout the site
   });
   await User.findByIdAndUpdate(userId, { token });
